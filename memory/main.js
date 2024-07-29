a49b88a5ebf64ee570bbd506e2dc027423f0261c
magriker <magken7@gmail.com>
2024-07-29 09:38:14 +0900

    Second change

--- memory/main.js
+++ memory/main.js
@@ -9,6 +9,8 @@ function creatCards() {
   let secondLine = [];
   let merge = [];
 
+  btn.style.display = "none";
+
   for (let i = 1; i <= 5; i++) {
     firstLine.push(i);
     secondLine.push(i);
@@ -42,17 +44,80 @@ const shaffleArray = function (array) {
   return shuffledArray;
 };
 
-function checkCards(e) {
-  hitcards.push(e.target.textContent);
+let hitcards = [];
+let correctEl = [];
+let targetEl = [];
+let mistake = [];
 
-  if (hitcards.length >= 2) {
-    console.log(hitcards);
-    hitcards = [];
+function checkCards(e) {
+  checkArray();
+  addNumber(e);
+
+  if (hitcards.length === 2 && hitcards[0] === hitcards[1]) {
+    console.log("一緒");
+    console.log(targetEl);
+    targetEl.map((e) => correctEl.push(e));
+    targetEl = [];
+    if (correctEl.length === 10) {
+      setTimeout(function () {
+        const clear = confirm("Clear the game!");
+        if (clear) {
+          window.location.reload();
+        }
+      }, 1500);
+    }
+    console.log(correctEl);
+    console.log(targetEl);
+  } else if (hitcards.length === 2 && hitcards[0] !== hitcards[1]) {
+    console.log("違う");
+    setTimeout(function () {
+      targetEl.map((e) => e.classList.remove("cardBack"));
+      targetEl = [];
+      mistake.push(1);
+      if (mistake.length === 3) {
+        const gameOver = confirm("Gameover!!\nClick OK!");
+        if (gameOver) {
+          window.location.reload();
+        }
+      }
+
+      console.log(mistake);
+      console.log(targetEl);
+    }, 1500);
   }
+  // if (hitcards.length <= 2) {
+  //   hitcards.push(e.target.textContent);
+  // }
+
+  // if (hitcards.length >= 3) {
+  //   hitcards = [];
+  // } else if (hitcards[0] === hitcards[1]) {
+  //   console.log(hitcards);
+  //   console.log("一緒！");
+  // }
 }
 
 function changeColor(e) {
   e.target.classList.add("cardBack");
 }
-let hitcards = [];
-let preValue = [];
+
+function checkArray() {
+  if (hitcards.length === 2) {
+    console.log(hitcards);
+    return (hitcards = []);
+  }
+}
+
+function addNumber(e) {
+  if (hitcards.length <= 1) {
+    hitcards.push(e.target.textContent);
+    changeColor(e);
+    targetEl.push(e.target);
+    console.log(hitcards);
+  }
+}
+
+function checkMistake(mistake) {
+  if (mistake.length === 3) {
+  }
+}
