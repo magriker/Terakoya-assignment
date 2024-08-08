8f31fb6a7f6318d34372583d86b29c6ec6cd6241
magriker <magken7@gmail.com>
2024-08-09 01:16:37 +0900

    react

---
+++ myreact/src/App.js
@@ -0,0 +1,8 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return <div>Hello app</div>;
+}
+
+export default App;
---
+++ myreact/src/App2.js
@@ -0,0 +1,5 @@
+function App2() {
+  return <div>Hello react</div>;
+}
+
+export default App2;
