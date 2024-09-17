36c978b3aa5c5374a4a0fe7c404fbc780b881142
magriker <magken7@gmail.com>
2024-09-18 00:22:35 +0900

    emotion-test

--- myreact/src/form/FormDiary.jsx
+++ myreact/src/form/FormDiary.jsx
@@ -1,3 +1,6 @@
+/** @jsxImportSource @emotion/react */
+
+import { css } from "@emotion/react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
@@ -7,46 +10,126 @@ const FormDiray = () => {
   const [diaryList, setDiaryList] = useState([]);
 
   const schema = yup.object().shape({
-    title: yup.string().required("タイトが必要です。"),
+    title: yup.string().required("タイトルが必要です。"),
     memo: yup.string(),
   });
 
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors },
   } = useForm({
+    defaultValues: {
+      date: "",
+      title: "",
+      memo: "",
+    },
     resolver: yupResolver(schema),
   });
 
-  const onSubmit = (data, e) => {
+  const onSubmit = (data) => {
     setDiaryList([...diaryList, data]);
-    console.log(diaryList);
-
-    e.preventDefault();
-    e.target.reset();
+    reset();
   };
 
+  const container = css`
+    width: 50%;
+    margin: 5rem auto;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+  `;
+
+  const formStyle = css`
+    display: flex;
+    flex-direction: column;
+  `;
+
+  const buttonStyle = css`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 6px 14px;
+    border-radius: 6px;
+    border: none;
+    margin-top: 5px;
+    background: #6e6d70;
+    box-shadow: 0px 0.5px 1px rgba(0, 0, 0, 0.1),
+      inset 0px 0.5px 0.5px rgba(255, 255, 255, 0.5),
+      0px 0px 0px 0.5px rgba(0, 0, 0, 0.12);
+    color: #dfdedf;
+  `;
+
+  const textareaStyle = css`
+    border-radius: 6px;
+    padding: 10px;
+    border: 2px solid #ccc;
+  `;
+
+  const titleStyle = css`
+    border-radius: 6px;
+    padding: 10px;
+    border: 2px solid #ccc;
+  `;
+
+  const labelStyle = css`
+    position: relative;
+    display: inline-block;
+    width: 200px;
+    height: 36px;
+    border: 2px solid #ccc;
+    border-radius: 6px;
+  `;
+
+  const inputStyle = css`
+    position: relative;
+    padding: 0 10px;
+    width: 200px;
+    height: 36px;
+    border: 0;
+    background: transparent;
+    box-sizing: border-box;
+    font-size: 14px;
+  `;
+
+  const errorStyle = css`
+    color: red;
+  `;
+
   return (
-    <>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        タイトル：
-        <input type="text" placeholder="タイトル" {...register("title")} />
-        <p className="error">{errors.title?.message}</p>
+    <div css={container}>
+      <form onSubmit={handleSubmit(onSubmit)} css={formStyle}>
+        日付：
+        <label htmlFor="" css={labelStyle}>
+          <input type="date" css={inputStyle} {...register("date")}></input>
+        </label>
         <br />
+        タイトル：
+        <input
+          css={titleStyle}
+          type="text"
+          placeholder="タイトル"
+          {...register("title")}
+        />
+        <p css={errorStyle}>{errors.title?.message}</p>
         <label htmlFor="memo">メモ：</label>
         <textarea
           id="memo"
           rows={10}
           cols={50}
           {...register("memo")}
+          css={textareaStyle}
         ></textarea>
-        <button type="submit">保存</button>
+        <button type="submit" css={buttonStyle}>
+          保存
+        </button>
       </form>
       <br />
       {diaryList.map((list, index) => (
         <div key={index}>
           <p>
+            日付：{list.date}
             <br />
             タイトル：{list.title}
             <br />
@@ -55,7 +138,7 @@ const FormDiray = () => {
           <br />
         </div>
       ))}
-    </>
+    </div>
   );
 };
 
