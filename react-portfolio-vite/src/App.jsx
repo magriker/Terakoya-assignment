37e51ceeab239997c1af9699b929587cf3e655c7
magriker <magken7@gmail.com>
2024-10-20 22:42:19 +0900

    commit

--- react-portfolio-vite/src/App.jsx
+++ react-portfolio-vite/src/App.jsx
@@ -1,5 +1,100 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import EditNoteIcon from "@mui/icons-material/EditNote";
+import { Container } from "@mui/material";
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import TextField from "@mui/material/TextField";
+import FormControl from "@mui/material/FormControl";
+
 function App() {
-  return <>Hello world</>;
+  return (
+    <>
+      {/* <Menu></Menu> */}
+      {/* <Memo></Memo> */}
+      <Memo></Memo>
+    </>
+  );
 }
+
+const Memo = () => {
+  return (
+    <>
+      <Container maxWidth="sm">
+        <h1>Memo App</h1>
+        <Button variant="outlined">新規作成</Button>
+        <Button variant="outlined">編集</Button>
+      </Container>
+      <Container maxWidth="sm">
+        <form action="">
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DemoContainer components={["DatePicker"]}>
+              <DatePicker label="Basic date picker" />
+            </DemoContainer>
+          </LocalizationProvider>
+
+          <TextField
+            id="outlined-basic"
+            fullWidth
+            label="タイトル"
+            variant="outlined"
+          />
+          <TextField
+            id="outlined-multiline-static"
+            label="メモ"
+            multiline
+            fullWidth
+            rows={8}
+          />
+        </form>
+      </Container>
+    </>
+  );
+};
+
+const Menu = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
+  };
+
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+      <List>
+        {["Memo"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <EditNoteIcon></EditNoteIcon>
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+    </Box>
+  );
+
+  return (
+    <div>
+      <Button onClick={toggleDrawer(true)}>Menu</Button>
+      <Drawer open={open} onClose={toggleDrawer(false)}>
+        {DrawerList}
+      </Drawer>
+    </div>
+  );
+};
 
 export default App;
